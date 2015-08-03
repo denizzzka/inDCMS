3e8d592e7154e2b7b032fa1d1b011c030a80d972
DarkRiDDeR <darkridderr@ya.ru>
2015-08-04 00:29:18 +0800

    add System addon

---
+++ source/system/models/index.d
@@ -0,0 +1,1 @@
+module indcms.system.models.index;
---
+++ source/system/system.d
@@ -0,0 +1,25 @@
+﻿module indcms.system;
+
+import vibe.d;
+
+import temple;
+
+static final class System
+{
+static:
+	private bool isInit = false;
+	private URLRouter urlRouter;
+
+	public void init()
+	{
+		if (this.isInit) return;
+		this.urlRouter = new URLRouter;
+		this.isInit = true;
+	}
+
+	public URLRouter getRouter ()
+	{
+		return this.urlRouter;
+	}
+}
+
